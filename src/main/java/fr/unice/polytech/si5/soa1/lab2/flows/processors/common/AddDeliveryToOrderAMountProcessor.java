2b3bf9f4bddc088073dcbc90e5bb14e2fc43ec71
Pierre Leca <pierre.leca@etu.unice.fr>
2015-11-15 15:47:51 +0100

    fix delivery amount

--- src/main/java/fr/unice/polytech/si5/soa1/lab2/flows/processors/common/AddDeliveryToOrderAMountProcessor.java
+++ src/main/java/fr/unice/polytech/si5/soa1/lab2/flows/processors/common/AddDeliveryToOrderAMountProcessor.java
@@ -2,6 +2,7 @@ package fr.unice.polytech.si5.soa1.lab2.flows.processors.common;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.w3c.dom.Node;
 
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.xpath.XPath;
@@ -16,9 +17,9 @@ public class AddDeliveryToOrderAMountProcessor implements Processor {
 
     public void process(Exchange exchange) throws Exception {
 
-        DOMSource deliveryDOM = exchange.getIn().getBody(DOMSource.class);
+        Node source = exchange.getIn().getBody(DOMSource.class).getNode();
 
-        double delivery = Double.parseDouble(xpath.evaluate("//deliveryPrice/text()", deliveryDOM));
+        double delivery = Double.parseDouble(xpath.evaluate("//deliveryPrice/text()", source));
         double amount = exchange.getIn().getHeader("order_amount", Double.class);
 
         exchange.getIn().setBody(delivery + amount);
